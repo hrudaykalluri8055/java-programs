8b4f5866b5ca7f50c59fc21800255b7a4dab1fab
Hruday Kalluri <hrudaykallurii@gmail.com>
2025-09-24 12:37:43 +0530

    Update and rename instancevar.java to localinstance.java

--- localinstance.java
+++ localinstance.java
@@ -2,6 +2,8 @@ class instance{
     int a = 10;//out the method
     public static void main(String[] args) {
         instance s = new instance(); // class name refarence varible = new class name();
-        System.out.print("instance varible: " + s.a);
+        int b=2;
+       int c=s.a+b;
+        System.out.print("instance varible: " + c);
     }
 }
