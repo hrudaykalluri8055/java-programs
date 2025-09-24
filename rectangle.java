2b0adda0b3bb7320fb29c56019b04231a820be11
Hruday Kalluri <hrudaykallurii@gmail.com>
2025-09-24 22:29:50 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,8 @@
+class a{
+    public static void main(String[] args) {
+        int length=10;
+        int breath=10;
+        int area = length * breath;
+        System.out.println("area of rectangular: " + area);
+    }
+}
