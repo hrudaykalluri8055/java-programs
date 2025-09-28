95ff23f88b0b1b1661f4d5335dc5dae96539907d
Hruday Kalluri <hrudaykallurii@gmail.com>
2025-09-28 13:41:55 +0530

    Add files via upload

---
+++ studentval.java
@@ -0,0 +1,18 @@
+class studen{
+    public static void main(String[] args) {
+        int id = 8055;
+        String name = "roy";
+        float marks = 901f;
+        char grade = 'A';
+        boolean result = true;
+        short rank = 8055;
+        long number = 7799461935l;
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Marks: " + marks);
+        System.out.println("Student Grade: " + grade);
+        System.out.println("Student Result: " + result);
+        System.out.println("Student Rank: " + rank);
+        System.out.println("Student Mobile number: " + number);
+    }
+}
